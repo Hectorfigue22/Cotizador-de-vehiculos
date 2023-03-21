2d4229973788233206d495c1e9dcb59234a751c9
Hector Figueroa <figueroahe_11@hotmail.com>
2023-03-20 23:24:40 -0300

    agregue el objeto vehiculos

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 
-let mensaje_precio = ""
+const carrito = []
+
+let vehículo = [{imagen: , codigo: 1, tipo: "Moto chica", precio_dia: 5000},
+                {imagen: , codigo: 2, tipo: "Moto grande", precio_dia: 7000},
+                {imagen: , codigo: 3, tipo: "Auto chico", precio_dia: 9000},
+                {imagen: , codigo: 4, tipo: "Auto mediano", precio_dia: 11000},
+                {imagen: , codigo: 5, tipo: "Auto grande", precio_dia: 13000}
+                {imagen: , codigo: 6, tipo: "Camioneta cerrada", precio_dia: 16000}
+                {imagen: , codigo: 7, tipo: "Camioneta con caja", precio_dia: 18000}
+                {imagen: , codigo: 8, tipo: "Camion chico", precio_dia: 20000}
+                {imagen: , codigo: 9, tipo: "Camion grande", precio_dia: 22000}]
+
+
+
+/*let mensaje_precio = ""
 let continuar = true
 const mensaje_inicial = "Ingrese el codigo de su producto \n" +
                         "A) Motos \n" +
@@ -44,4 +58,4 @@ function preguntar_precio() {
         continuar = confirm("¿Desea conocer el precio de otro vehículo?")
     }
 
-}
+}*/
