b49ffe21a20abd452c3fc5437ac555e8a6c3626d
Hector Figueroa <figueroahe_11@hotmail.com>
2023-03-23 22:47:22 -0300

    agregue funciones, objetos y array

---
+++ js/class.alquiler.js
@@ -0,0 +1,21 @@
+class alquiler{
+    constructor(carrito) {
+        this.carrito = carrito
+    }
+
+    obtener_subtotal() {
+        if (this.carrito.length > 0) {
+            return this.carrito.reduce((acumulador, vehiculo)=> acumulador + acumulador + vehiculo.precio_dia, 0)
+        }else {
+            return `Error: Por favor vuelva a intentarlo.`
+        }
+    }
+
+    terminarCompra() {
+        if (this.obtener_subtotal() !== `Error: Por favor vuelva a intentarlo.`) {
+            return `Su pago fue confirmado con un importe de $ ${this.obtener_subtotal()} \n Muchas gracias por confiar en nosotros.`
+        }else {
+            return `Ocurrio un problema, intentelo nuevamente en unos minutos.`
+        } 
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,61 +1,70 @@
-
 const carrito = []
 
-let vehículo = [{imagen: , codigo: 1, tipo: "Moto chica", precio_dia: 5000},
-                {imagen: , codigo: 2, tipo: "Moto grande", precio_dia: 7000},
-                {imagen: , codigo: 3, tipo: "Auto chico", precio_dia: 9000},
-                {imagen: , codigo: 4, tipo: "Auto mediano", precio_dia: 11000},
-                {imagen: , codigo: 5, tipo: "Auto grande", precio_dia: 13000}
-                {imagen: , codigo: 6, tipo: "Camioneta cerrada", precio_dia: 16000}
-                {imagen: , codigo: 7, tipo: "Camioneta con caja", precio_dia: 18000}
-                {imagen: , codigo: 8, tipo: "Camion chico", precio_dia: 20000}
-                {imagen: , codigo: 9, tipo: "Camion grande", precio_dia: 22000}]
+let vehiculo = [{codigo: 1, tipo: "Moto chica", precio_dia: 5000},
+                {codigo: 2, tipo: "Moto grande", precio_dia: 7000},
+                {codigo: 3, tipo: "Auto chico", precio_dia: 9000},
+                {codigo: 6, tipo: "Auto Electrico chico", precio_dia: 11000},
+                {codigo: 4, tipo: "Auto mediano", precio_dia: 13000},
+                {codigo: 7, tipo: "Auto Electrico mediano", precio_dia: 15000},
+                {codigo: 5, tipo: "Auto grande", precio_dia: 17000},
+                {codigo: 8, tipo: "Auto Electrico grande", precio_dia: 19000},
+                {codigo: 9, tipo: "Camioneta cerrada", precio_dia: 21000},
+                {codigo: 10, tipo: "Camioneta con caja", precio_dia: 23000},
+                {codigo: 11, tipo: "Camion chico", precio_dia: 25000},
+                {codigo: 12, tipo: "Camion grande", precio_dia: 27000}]
 
 
+const mensaje_inicial = "Seleccione un código numérico correspondiente a su vehículo: "
 
-/*let mensaje_precio = ""
-let continuar = true
-const mensaje_inicial = "Ingrese el codigo de su producto \n" +
-                        "A) Motos \n" +
-                        "B) Autos \n" +
-                        "C) Autos Electricos \n" +
-                        "D) Caminetas \n" +
-                        "E) Camiones \n"
-
-function iniciar_consulta() {
-    let seleccion = prompt(mensaje_inicial).toUpperCase().trim()
-    
-    if (seleccion !== "A" && seleccion !== "B" && seleccion !== "C" && seleccion !== "D" && seleccion !== "E" ){
-        alert("Debes ingresar un código válido para tu producto")
-    } else{
-        switch(seleccion){
-            case "A":
-                mensaje_precio = "El precio de la moto es U$D 5000"
-                break
-            case "B":
-                mensaje_precio = "El precio del auto es U$D 10000"
-                break
-            case "C":
-                mensaje_precio = "El precio del auto electrico es U$D 15000"
-                break
-            case "D":
-                mensaje_precio = "El precio de la camioneta es U$D 20000"
-                break
-            case "E":
-                mensaje_precio = "El precio de la moto es U$D 25000"
-                break
-            default:
-        }
-        alert(mensaje_precio)
-    }
-
+function buscarVehiculo(codigo) {
+    let resultado = vehiculo.find((vehiculo)=> vehiculo.codigo === parseInt(codigo))
+    return resultado
 }
 
-function preguntar_precio() {
+function mirarCarrito() {
+    console.table(carrito)
+}
 
-    while(continuar) {
-        iniciar_consulta()
-        continuar = confirm("¿Desea conocer el precio de otro vehículo?")
+function finCompra() {
+    if (carrito.length > 0) {
+        const arrendamiento = new alquiler(carrito)
+        alert(`El costo de la compra es de $ ${arrendamiento.obtener_subtotal()}`)
+        let respuesta = confirm("¿Desea confirmar la compra?")
+        if(respuesta) {
+            alert(arrendamiento.terminar_compra())
+            carrito.length = 0
+        }
+    }else {
+        console.warn("No hay vehiculos en el carrito.")
     }
+}
 
-}*/
+function rentaAcar(){
+    let codigo = prompt(mensaje_inicial)
+        if (!parseInt(codigo)) {
+            alert("Error en el codigo ingresado")
+            let respuesta = confirm("¿Desea intentarlo nuevamente?")
+            if(respuesta){
+                rentaAcar()
+            }
+            return  
+        }
+        
+    let vehiculo_elegido = buscarVehiculo(codigo)
+    if (vehiculo_elegido !== undefined) {
+        alert(`${vehiculo_elegido.tipo} El vehiculo ${vehiculo_elegido.precio_dia} a sido agegado a su carrito`)
+        carrito.push(vehiculo_elegido)
+        let respuesta = confirm("¿Desea comprar otro vehiculo?")
+        if (respuesta){
+                rentaAcar()
+        }else
+            finCompra()
+    }else {
+        alert("Error en el codigo ingresado")
+            let respuesta = confirm("¿Desea intentarlo nuevamente?")
+            
+    }
+            
+}
+
+// TENDRIA QUE TERMINAR SIENDO UNA PAGINA DE ALQUILER DE VEHICULOS POR AHORA VENTA DE VEHICULOS
